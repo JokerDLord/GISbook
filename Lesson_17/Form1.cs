8b52e953dbc679e3531f51593f7af5f7d82a2de2
1723663554 <joker.lord@foxmail.com>
2019-11-28 01:28:47 +0800

    唯一值专题地图

---
+++ Lesson_17/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson_17
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            gisPanel1.document.layers[0].Thematic.InsideColor = Color.Red;
+            gisPanel1.UpdateMap();
+        }
+    }
+}
